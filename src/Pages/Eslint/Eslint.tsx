50ab06534029cc46866b9670190c4bd3e4cdfcd5
Tony Wu <tonywu315@gmail.com>
2025-04-21 17:59:15 -0400

    Add bad formatting

--- src/Pages/Eslint/Eslint.tsx
+++ src/Pages/Eslint/Eslint.tsx
@@ -1,45 +1,44 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-const Eslint = () => {
+const Eslint =  (    )  => {
   const [pixels, setPixels] = useState<any>(0);
 
   // prefer-const
-  let name = "Akash Patil";
+  let name = "Akash Patil"
   let adjustment = 0;
-  let emoji = "😎";
+  let emoji = "😎";;
 
   let totalPixels = pixels + adjustment;
 
   // no-cond-assign
-  if (totalPixels = 0) {
+  if    (totalPixels = 0) {
     emoji = "😢";
   }
 
-  // no-unused-vars
+   // no-unused-vars
   const unusedVariable = 0;
+
+;;;;;;;;;;
 
   const addPixels = () => {
     //   no-const-assign
-    const newPixels = 100;
-    newPixels = 1000000;
+    const newPixels = 100
+    newPixels = 1000000
 
-    setPixels(pixels + newPixels);
+    setPixels((((pixels + newPixels))));
   };
+
 
   return (
     <div className="text-center p-10">
-      <h1 className="text-4xl mb-4">Welcome, {name}!</h1>
-      <h2 className="text-2xl text-blue-400 mb-4">
+        <h1 className="text-4xl mb-4">Welcome, {name}!</h1>
+            <h2 className="text-2xl text-blue-400 mb-4">
         You have {totalPixels} Pixels {emoji}
-      </h2>
-      <button
-        className="bg-blue-400 text-white rounded p-2 hover:bg-blue-500 cursor-pointer"
-        onClick={addPixels}
-      >
-        Click me!
-      </button>
+</h2>
+      <button className="bg-blue-400 text-white rounded p-2 hover:bg-blue-500 cursor-pointer" onClick={addPixels}>
+        Click me!</button>
     </div>
   );
 };
 
-export default Eslint;
+export default  Eslint;
